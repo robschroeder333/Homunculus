05def45601c8cf2e87dd81f5121b1c85bafe9053
robschroeder333 <theguyonthecouch7@gmail.com>
2019-10-17 13:53:37 -0700

    init

---
+++ Homunculus/Alive.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homunculus
+{
+    public enum Need { Breath, Water, Food, Sleep, Happiness}
+    abstract class Alive
+    {
+        public int Health { get; private set; }
+        public int Age { get; private set; }
+        public int Size { get; private set; }
+        public Dictionary<string, int> Resources { get; private set; }
+        public Dictionary<Need, int> Needs { get; private set; }
+
+        public virtual void Die(Alive subType) 
+        {
+            //create new object with rescources carried over
+        }
+        public virtual void Grow() 
+        {
+            Size++;
+        }
+        public virtual void Breed() { }
+    }
+}
